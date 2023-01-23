4a0e2fa767bc14f0a93f6ee3a1238e07c7f24dde
Decorce Colin <82278211+Coldcolin@users.noreply.github.com>
2023-01-23 17:33:12 +0100

    Update data.js

--- Components/data.js
+++ Components/data.js
@@ -16,5 +16,15 @@ export const products = [
         id: 3,
         img: "/pickaStudio.png",
         link: "https://pickastudio-34376.web.app/"
-    }
+    },
+    {
+        id: 4,
+        img: "https://res.cloudinary.com/coldcolin/image/upload/v1674491296/screencapture-cold-commerce-vercel-app-2023-01-23-17_22_34_vcqfoq.png",
+        link: "https://cold-commerce.vercel.app/"
+    },
+    {
+        id: 5,
+        img: "https://res.cloudinary.com/coldcolin/image/upload/v1674491311/screencapture-cold-todo-app-onrender-2023-01-23-17_25_08_uyqbf8.png",
+        link: "https://cold-todo-app.onrender.com"
+    },
 ]
